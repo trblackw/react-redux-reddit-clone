62afd42646316273cb58b3414e9d38fa8e823ab0
Tucker <trblackw@ncsu.edu>
2018-09-28 19:09:52 -0400

    created header component to house nav

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "../img/reddit-logo.png";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => (
+  <NavUl>
+    <li>
+      <NavLink to="/" activeStyle={{ color: "red" }}>
+        Home
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="#">Discover</NavLink>
+    </li>
+    <li>
+      <NavLink to="#">other stuff</NavLink>
+    </li>
+  </NavUl>
+);
+
+export default () => {
+  return (
+    <div>
+      <Header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Nav />
+      </Header>
+    </div>
+  );
+};
+
+const Header = styled.header`
+  background-color: #eee;
+  height: auto;
+  padding: 20px;
+  color: white;
+  border-bottom: 1px solid black;
+`;
+
+const NavUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin: 1em;
+  list-style: none;
+  li {
+    margin-right: 0.6em;
+  }
+  li a {
+    text-decoration: none;
+    color: black;
+  }
+`;
